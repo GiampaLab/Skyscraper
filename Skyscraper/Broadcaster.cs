f758baf1d1758137fca0617a9077dca4f8f255f2
Giampaolo Paderno <giampaolo.paderno@gmail.com>
2015-02-04 22:01:06 +0000

    first commit

---
+++ Skyscraper/Broadcaster.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Skyscraper
+{
+    public class Broadcaster
+    {
+        private readonly static Lazy<Broadcaster> _instance =
+            new Lazy<Broadcaster>(() => new Broadcaster());
+        // We're going to broadcast to all clients a maximum of 25 times per second
+        private readonly TimeSpan BroadcastInterval =
+            TimeSpan.FromMilliseconds(40);
+        private readonly IHubContext _hubContext;
+        private Timer _broadcastLoop;
+        private BasicShapeModel _model;
+        private bool _modelUpdated;
+        public Broadcaster()
+        {
+            // Save our hub context so we can easily use it 
+            // to send to its connected clients
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<SkyscraperHub>();
+            _model = new BasicShapeModel();
+            _modelUpdated = false;
+            // Start the broadcast loop
+            _broadcastLoop = new Timer(
+                BroadcastShape,
+                null,
+                BroadcastInterval,
+                BroadcastInterval);
+        }
+        public void BroadcastShape(object state)
+        {
+            // No need to send anything if our model hasn't changed
+            if (_modelUpdated)
+            {
+                // This is how we can access the Clients property 
+                // in a static hub method or outside of the hub entirely
+                _hubContext.Clients.AllExcept(_model.LastUpdatedBy).updateShape(_model);
+                _modelUpdated = false;
+            }
+        }
+        public void UpdateShape(BasicShapeModel clientModel)
+        {
+            _model = clientModel;
+            _modelUpdated = true;
+        }
+        public static Broadcaster Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
---
+++ Skyscraper/MouseConstraint.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skyscraper
+{
+    class MouseConstraint
+    {
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+        [JsonProperty("constraint")]
+        public Constraint Constraint { get; set; }
+
+        public Body DragBody { get; set; }
+    }
+}

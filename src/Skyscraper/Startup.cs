f54baf460527815b43c7b62c7078934072066da1
Giampaolo Paderno <giampaolo.paderno@gmail.com>
2015-07-05 16:58:33 +0100

    new dnvm version update

--- src/Skyscraper/Startup.cs
+++ src/Skyscraper/Startup.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Cors.Core;
+using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 using SkyscraperCore;
-using System;
 
 namespace Skyscraper
 {
     public class Startup
     {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //Add all SignalR related services to IoC.
+            services.Configure<CorsOptions>(options =>
+            {
+                options.AddPolicy("AllowEverything", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+                });
+            });
             services.AddSignalR();
             services.AddMvc();
             services.AddScoped<IGameFactory, GameFactory>();
@@ -19,7 +32,8 @@ namespace Skyscraper
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Use(async (context, next) => {
+            app.Use(async (context, next) =>
+            {
                 context.Response.Headers.Append("Access-Control-Allow-Origin", "http://192.168.0.3:8088");
                 context.Response.Headers.Add("Access-Control-Allow-Headers", new[] { "Content-Type, x-xsrf-token" });
                 context.Response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
@@ -33,14 +47,13 @@ namespace Skyscraper
                 // By default this will allow all origins. You can 
                 // configure the set of origins and/or http verbs by
                 // providing a cors options with a different policy.
-                //map.UseCors(CorsOptions.AllowAll);
-                
+                map.UseCors("AllowEverything");
+
                 // Run the SignalR pipeline. We're not using MapSignalR
                 // since this branch already runs under the "/signalr"
                 // path.
                 map.RunSignalR();
             });
-            
         }
     }
 }
---
+++ src/Skyscraper/client/config.js
@@ -0,0 +1,5 @@
+angular.module('config', [])
+
+.constant('constants', {signalREndpoint:'http://localhost:8000/signalr'})
+
+;
